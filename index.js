a262851f19ce3fef95cd94f4060a731d22311f80
athtrdo <ajitrih7@gmail.com>
2022-08-04 01:30:09 +0700

    Add Impelementation CRUD

--- index.js
+++ index.js
@@ -1,93 +1,175 @@
-
-
 const express = require('express')
+const db = require('./connection/db')
 const app = express()
 const port = 5000
 const isLogin = true
 let addProjects = [] 
 
+// Testing connection database
+db.connect((error,client,done) => {
+  if (error) {
+    return console.log(error);
+  }
+  console.log('Connection db success');
+  done()
+})
 
 app.set('view engine', 'hbs')
 app.use('/public', express.static(__dirname + '/public'))
 app.use(express.urlencoded({extended: false}))
 
+
+
 app.get('/', (req,res) => {
-    res.render('index',{addProjects});
-});
+
+  
+  db.connect((err,client, done) => {
+    if (err) throw err
+
+    const query = `SELECT * FROM tb_projects ORDER BY id ASC`
+
+
+    client.query(query,(err,result) => {
+      if (err) throw err
+
+      const data = result.rows;
+
+      const newProjects = data.map((project) => {
+        const newProject = {
+          ...project,
+          Distime: getDistime(project.start_date, project.end_date),
+        isLogin: isLogin
+        }
+        return newProject
+      })
+      res.render('index',{addProjects: newProjects})
+
+      })
+      done()
+    
+    })
+  })
 
 app.get('/contact-me', (req, res) => {
     res.render('contact-me')
 })
-app.post('/Add-project/add', (req, res) => {
+app.post('/Project/add', (req, res) => {
     const {name, startDate,endDate,description,Tech} = req.body
-    addProjects.push({
-        name,
-        startDate,
-        endDate,
-        start: new Date(startDate).toLocaleDateString(),
-        end: new Date(endDate).toLocaleDateString(),
-        description,
-        Distime: getDistime(startDate,endDate),
-        isLogin,
-        Tech,
-    })
+    db.connect((err,client,done) => {
+      if(err) throw err
+      
+      const query = `INSERT INTO tb_projects (name,start_date,end_date,description,"Tech")
+      VALUES ('${name}','${startDate}','${endDate}','${description}','{${Tech}}')`;
 
-    res.redirect('/')
-})
+      client.query(query, (err) =>{
+        if (err) throw err
+        done()
 
-app.get('/Add-Project/add', (req, res) => {
+        res.redirect('/')
+     })
+   })
+ })
+
+
+app.get('/Project/add', (req, res) => {
    if(!isLogin) {
       res.redirect('/auth')
       return
    }
-  res.render('Add-Project')
+  res.render('Project')
 })
 
-app.get('/delete-project/:idx', (req,res) => {
-  const idx = req.params.idx // idx == index
-  addProjects.splice(idx,1)
-  res.redirect('/')
+app.get('/delete-project/:id', (req,res) => {
+  const id = req.params.id
+
+  db.connect((err, client, done) => {
+    if(err) throw err;
+
+    const query =`DELETE FROM tb_projects WHERE id = ${id};`
+    client.query(query, (err) => {
+      
+      if(err) throw err
+      done()
+      
+      res.redirect('/')
+    })
+    
+  }) 
 })
 
-app.post('/Add-Project/edit/:id', (req,res) => {
-  const {id} = req.params
-  const {name,startDate,endDate,description,Tech}= req.body
-
-  addProjects[id] = {
-    ...addProjects[id],
-    name,
-    startDate,
-    endDate,
-    description,
-    Tech,
-  }
-  res.redirect('/')
-})
-
-app.get('/Add-Project/edit/:id', (req, res) => {
+app.post('/Project/edit/:id', (req,res) => {
   
-    const { id } = req.params
-    const project = addProjects[id]
-    res.render('Edit-project', { project: { ...project, id } })
+  const {name, startDate,endDate,description,Tech} = req.body
+
+  const id = req.params.id
+
+  db.connect((err,client,done) => {
+    if(err) throw err
+    
+    const query = `UPDATE public.tb_projects SET 
+    name ='${name}',
+    start_date = '${startDate}',
+    end_date ='${endDate}',
+    description = '${description}',
+    "Tech" ='{${Tech}}'
+    WHERE id = ${id};`
+
+    
+
+    client.query(query, (err) =>{
+      if (err) throw err
+      done()
   
+    res.redirect('/');
+   })
+ })
 })
 
-app.get('/DetailProject/:index', (req, res) => {
-    const index = req.params.index
-    const project = addProjects[index]
-    const newProject = {
-        ...project,
-        name,
-        startDate,
-        endDate,
-        description,
-        Tech,
-        isLogin: isLogin}
-    console.log(newProject);
-    res.render('DetailProject',{project : newProject})
-   
-})
+app.get("/Project/edit/:id", (req, res) => {
+  const id = req.params.id;
 
+  db.connect((err, client, done) => {
+    if (err) throw err;
+
+    const query = `SELECT * FROM tb_projects WHERE id = ${id}`;
+
+    client.query(query, (err, result) => {
+      if (err) throw err;
+
+      done();
+      // console.log(result.rows[0]);
+      res.render("Edit-project", { Data: result.rows[0] });
+    });
+  });
+  // console.log("data menta updatep:", Data);
+});
+
+
+app.get('/details-project/:id', (req, res) => {
+  const id = req.params.id;
+  // const { title, start_date, end_date, description, technologies } = req.body;
+  db.connect((err, client, done) => {
+    if (err) console.log(err);
+    const query = `SELECT * FROM tb_projects WHERE id = ${id}`;
+
+    client.query(query, (err, result) => {
+      done();
+      if (err) throw err;
+
+      let project = result.rows[0]
+
+        project = {
+          ...project,
+          Distime: getDistime(project.start_date, project.end_date),
+          start_date: new Date(project.start_date),
+          end_date: new Date(project.end_date),
+        }
+
+      
+      res.render("details-project", { project });
+    });
+  });
+});
 
 // Not found route custome
 
